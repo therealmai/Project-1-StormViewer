b945e222ac73cae74d15b8ea86eac684a06f4f2d
Jomar Leano <jomar35leano@gmail.com>
2022-10-06 19:37:19 +0800

    connected image view to controller

--- Project 1 StormViewer/DetailViewController.swift
+++ Project 1 StormViewer/DetailViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
 
